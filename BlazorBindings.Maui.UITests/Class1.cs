43aac1660be2ba82dfa593685b1ee4c27a51cfd7
Oleksandr Liakhevych <liakh.oleksandr32@gmail.com>
2022-09-27 23:59:04 +0300

    wip

--- BlazorBindings.Maui.UITests/Class1.cs
+++ BlazorBindings.Maui.UITests/Class1.cs
@@ -1,7 +1,22 @@
-﻿namespace BlazorBindings.Maui.UITests
+﻿using Microsoft.Maui.Automation.Driver;
+using NUnit.Framework;
+
+namespace BlazorBindings.Maui.UITests
 {
     public class Class1
     {
+        [Test]
+        public async Task Test()
+        {
+            var driver = new AppDriverBuilder()
+                .AppFilename("C:\\Users\\master\\source\\repos\\MobileBlazorBindings\\samples\\ControlGallery\\bin\\Debug\\net6.0-android\\com.companyname.controlgallery-Signed.apk")
+                .Device("pixel_3a_-_api_31")
+                .Build();
 
+            await driver.Start();
+
+            var screenshot = "C:\\Users\\master\\source\\repos\\MobileBlazorBindings\\BlazorBindings.Maui.UITests\\test.png";
+            await driver.Screenshot(screenshot);
+        }
     }
 }
