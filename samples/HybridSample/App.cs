7d0bcf7aaf667e5e08a7f544c8a3aa0e0ee00d9b
Oleksandr Liakhevych <liakh.oleksandr32@gmail.com>
2022-09-27 13:10:56 +0300

    Add BlazorWebView sample

---
+++ samples/HybridSample/App.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+using BlazorBindings.Maui;
+
+namespace HybridSample
+{
+    public class App : Application
+    {
+        public App(MauiBlazorBindingsRenderer renderer)
+        {
+            renderer.AddComponent<MainPage>(this);
+        }
+    }
+}
---
+++ samples/HybridSample/MauiProgram.cs
@@ -0,0 +1,30 @@
+﻿using BlazorBindings.Maui;
+using HybridSample.Data;
+
+namespace HybridSample
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .UseMauiBlazorBindings()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                });
+
+            builder.Services.AddMauiBlazorWebView();
+#if DEBUG
+            builder.Services.AddBlazorWebViewDeveloperTools();
+#endif
+
+            builder.Services.AddSingleton<WeatherForecastService>();
+            builder.Services.AddSingleton<CounterState>();
+
+            return builder.Build();
+        }
+    }
+}
