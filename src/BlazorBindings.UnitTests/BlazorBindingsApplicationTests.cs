68c4078209767e63ef26f04c0523c0428090fc7b
Oleksandr Liakhevych <liakh.oleksandr32@gmail.com>
2023-02-07 22:27:06 +0200

    Tests WIP

---
+++ src/BlazorBindings.UnitTests/BlazorBindingsApplicationTests.cs
@@ -0,0 +1,23 @@
+﻿using BlazorBindings.Maui;
+using BlazorBindings.UnitTests.Components;
+using Microsoft.AspNetCore.Components;
+using NUnit.Framework;
+
+namespace BlazorBindings.UnitTests
+{
+    public class BlazorBindingsApplicationTests
+    {
+        [Test]
+        public void SetsTheMainPage_ContentPage()
+        {
+            var application = CreateApplication<PageContent>();
+            PageContent.ValidateContent(application.MainPage);
+        }
+
+
+        private BlazorBindingsApplication<T> CreateApplication<T>() where T : IComponent
+        {
+            return new BlazorBindingsApplication<T>(TestServiceProvider.Create());
+        }
+    }
+}
